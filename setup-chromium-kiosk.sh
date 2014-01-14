36118f4af39abd33b6534d83b6a44e02ea6ae155
Johan Andersson <johan-spam@andersson.net>
2014-01-14 21:35:00 +0100

    Adds setup script to run post raspi-config / initial boot of raspian to setup kiosk

---
+++ setup-chromium-kiosk.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+if [ "$UID" -ne 0 ]
+	then echo "Please run $0 as root"
+	exit
+fi
+
+dir=`dirname $0`
+pushd $dir 1>/dev/null
+ 
+echo Running apt-get update...
+apt-get update -qq --force-yes
+echo Running apt-get dist-upgrade...
+apt-get dist-upgrade -qq --force-yes
+
+echo Setting up cron-apt
+apt-get install cron-apt -qq --force-yes
+echo "0 1 * * * root test -x /usr/sbin/cron-apt && /usr/sbin/cron-apt" > /etc/cron.d/cron-apt
+
+echo Using apt-get to install matchbox, chromium, etc
+apt-get install matchbox chromium x11-xserver-utils ttf-mscorefonts-installer sqlite3 libnss3 unclutter -qq --force-yes
+
+if [ -f /etc/rc.local ]; then
+	echo Moving existing /etc/rc.local to /etc/rc.local.bak
+	mv /etc/rc.local /etc/rc.local.bak
+fi
+
+echo Creating rc.local to start X as user $SUDO_USER using our xinitrc
+sed -e "s/KIOSKUSER=pi/KIOSKUSER=$SUDO_USER/g" etc/rc.local > /etc/rc.local
+echo Moving /boot/config.txt to /boot/config.txt.bak
+if [ -f /boot/config.txt ]; then
+	echo Moving existing /boot/config.txt to /boot/config.txt.bak
+	mv /boot/config.txt /boot/config.txt.bak
+fi
+cp boot/* /boot/
+echo Replacing config.txt with one from this repo
+mv /boot/config.template.txt /boot/config.txt
+
+# Install telldus tdtool and telldusd for Tellstick if device is present
+./setup-tellstick.sh
+
+echo "Done!"
+popd 1>/dev/null
+
+while true; do
+	read -p "Do you wish to reboot?" yn
+	case $yn in
+		[Yy]* ) shutdown -r now; break;;
+		[Nn]* ) exit;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
