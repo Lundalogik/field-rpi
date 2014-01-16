2f9dc431ce9b4830ea74593b44ae9785644b15d8
Johan Andersson <johan-spam@andersson.net>
2014-01-16 09:42:34 +0100

    Adds script for changing the URL displayed in the browser

---
+++ change-url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+newurl=$1
+
+if [ -z "$newurl" ]; then
+	echo "Enter the URL to change to:"
+	read newurl
+fi
+
+echo "Updating /boot/starturl.txt with URL $newurl"
+sudo echo $newurl > /boot/starturl.txt
+
+echo "Restarting chromium"
+sudo killall chromium
+
+echo Done
